aab60ce8189d517c6e710c796aee9b8e47bee778
Lana Alekseeva <107322490+schoegar@users.noreply.github.com>
2023-10-07 18:15:38 +0100

    New functions

--- app/src/points.py
+++ app/src/points.py
@@ -20,3 +20,14 @@ def get_mid_point(point1, point2):
     }
 
     return middle_point
+
+def make_decart_point(x, y):
+    return {"x": x, "y": y}
+
+
+def get_x(point):
+    return point["x"]
+
+
+def get_y(point):
+    return point["y"]
